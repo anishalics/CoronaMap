c334824d679fc631ba3f455d2396a24ddb7e46ab
Anish Ali <anishaliofficial@gmail.com>
2022-04-29 16:31:13 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function updateMap(){
+    fetch("/data.json")
+    .then(response => response.json())
+    .then(rsp => {
+        // console.log(rsp.data);
+        rsp.data.forEach(element => {
+            latitude = element.latitude;
+            longitude = element.longitude;
+            cases = element.infected;
+            
+            //Mark on the map
+            const marker = new mapboxgl.Marker({
+                draggable: false,
+                color: `rgba(255, 0, 0, ${cases/1000})`,
+                })
+                .setLngLat([longitude, latitude])
+                .addTo(map);
+        });
+    })
+    // console.log("Updating data...");
+}
+// setInterval(updateMap, 2000);
+updateMap();
